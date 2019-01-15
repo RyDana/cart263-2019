3656fbddac049e0ee92d530c3cde450336999154
Dana Ryashy <danriachi@hotmail.com>
2019-01-15 12:07:12 -0500

    As2: multiple food moves randomly

--- assignments/assignment2/js/Food.js
+++ assignments/assignment2/js/Food.js
@@ -10,10 +10,13 @@ class Food extends Agent {
   // Pass arguments on to the super() constructor (e.g. for Agent)
   // Also set a minimum and maximum size for this food object which it
   // will vary between when it resets
-  constructor(x,y,minSize,maxSize) {
+  constructor(x,y,minSize,maxSize,maxSpeed) {
     super(x,y,random(minSize,maxSize),'#55cccc');
     this.minSize = minSize;
     this.maxSize = maxSize;
+    this.maxSpeed = maxSpeed;
+    this.vx = random(-maxSpeed,maxSpeed);
+    this.vy = random(-maxSpeed,maxSpeed);
   }
 
   // reset()
@@ -24,5 +27,23 @@ class Food extends Agent {
     this.x = random(0,width);
     this.y = random(0,height);
     this.size = random(this.minSize,this.maxSize);
+  }
+
+  //update()
+  //
+  //Updates position of food, constraint to canvas
+  //randomly chages velocity of Food
+  update(){
+    if(random()<0.2){
+      this.vx = random(-this.maxSpeed,this.maxSpeed);
+      this.vy = random(-this.maxSpeed,this.maxSpeed);
+    }
+
+    this.x += this.vx;
+    this.y += this.vy;
+
+    this.x = constrain(this.x,0,width);
+    this.y = constrain(this.y,0,height);
+
   }
 }
--- assignments/assignment2/js/script.js
+++ assignments/assignment2/js/script.js
@@ -17,10 +17,12 @@ const AVATAR_MAX_SIZE = 64;
 const AVATAR_SIZE_LOSS_PER_FRAME = 1;
 const FOOD_MIN_SIZE = 5;
 const FOOD_MAX_SIZE = 100;
+const FOOD_MAX_SPEED = 5;
+const AMOUNT_OF_FOOD = 20;
 
 // Variables to store the two key objects
 let avatar;
-let food;
+let food = [];
 
 
 // preload()
@@ -38,8 +40,10 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
-  avatar = new Avatar(mouseX,mouseY,AVATAR_MAX_SIZE,AVATAR_SIZE_LOSS_PER_FRAME)
-  food = new Food(random(0,width),random(0,height),FOOD_MIN_SIZE,FOOD_MAX_SIZE);
+  avatar = new Avatar(mouseX,mouseY,AVATAR_MAX_SIZE,AVATAR_SIZE_LOSS_PER_FRAME);
+  for(let i=0;i<AMOUNT_OF_FOOD;i++){
+    food.push(new Food(random(0,width),random(0,height),FOOD_MIN_SIZE,FOOD_MAX_SIZE,FOOD_MAX_SPEED));
+  }
   noCursor();
 }
 
@@ -54,9 +58,13 @@ function draw() {
   background(0);
 
   avatar.update();
-  if (avatar.collide(food)) {
-    avatar.eat(food);
+  for(let i=0;i<food.length;i++){
+    food[i].update();
+    if (avatar.collide(food[i])) {
+      avatar.eat(food[i]);
+    }
+    food[i].display();
   }
+
   avatar.display();
-  food.display();
 }
